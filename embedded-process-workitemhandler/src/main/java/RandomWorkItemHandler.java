d3895528b6d03e5db3bc47260ed0132e25007535
Anushakrao <anukrao94@gmail.com>
2019-08-20 14:15:57 +0530

    custom workitem

---
+++ embedded-process-workitemhandler/src/main/java/RandomWorkItemHandler.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+import org.drools.core.process.instance.WorkItemHandler;
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemManager;
+
+public  class RandomWorkItemHandler implements WorkItemHandler  {
+
+	@Override
+	public void abortWorkItem(WorkItem workItem, WorkItemManager manager) {
+		// TODO Auto-generated method stub
+		manager.abortWorkItem(workItem.getId());
+		
+	}
+
+	@Override
+	public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
+		// TODO Auto-generated method stub
+		
+		Random rand = new Random();
+	    int  n = rand.nextInt(50) + 1;
+	    
+	    Object name = workItem.getParameter("Message");
+	    System.out.println("Hi " + name + ", your random number is: " + n);
+
+	    manager.completeWorkItem(workItem.getId(), null);
+		
+	}
+	
+	
+
+}
